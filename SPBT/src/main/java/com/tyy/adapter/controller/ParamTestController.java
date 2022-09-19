15ae309d18effe8096eb4066dc1abb9cfed8951c
taoyuanyuan <1936184791@qq.com>
2022-09-19 16:32:19 +0800

    tmp

--- SPBT/src/main/java/com/tyy/adapter/controller/ParamTestController.java
+++ SPBT/src/main/java/com/tyy/adapter/controller/ParamTestController.java
@@ -1,6 +1,9 @@
 package com.tyy.adapter.controller;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 
 /**
  * @Date 2022/9/19 16:09
@@ -12,8 +15,15 @@ public class ParamTestController {
 
 
     @GetMapping("/get/{name}")
-    public String testGet(@PathVariable("name") String name,
-                          @RequestParam("addr") String address) {//当入参名字不一样时，使用@RequestParam，相同可以不加
+    public String testGet(@PathVariable("name") String name,//使用@PathVariable获取路径参数
+                          @RequestParam("addr") String address) {//当入参名字不一样时，使用@RequestParam，相同可以不加！！！
+        System.out.println(name);
+        return "ok";
+    }
+
+    @GetMapping("/date")//@DateTimeFormat(pattern = "yyyy-MM-dd") 可以反序列化一个LocalDate的入参类型
+    public String test2(@DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date, String name) {
+        System.out.println(date);
         System.out.println(name);
         return "ok";
     }
